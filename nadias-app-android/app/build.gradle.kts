9acf6533da6250f2537baff8c757f86c151a8cb6
mounirakarkach04-pixel <mounirakarkach04@gmail.com>
2026-09-02 11:34:25 +0200

    build(nadia): configure Android app module

---
+++ nadias-app-android/app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.nadias.familyapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.nadias.familyapp"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
